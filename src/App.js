0dc3cd2b9595466b18c95a92e0f708825e54ee22
darkysadow <alexsmithshow@gmail.com>
2022-07-02 20:22:07 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Body from './components/Body';
+import './scss/style.scss'
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
