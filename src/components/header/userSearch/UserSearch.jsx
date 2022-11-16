2be7636cebfe7ec7a6bebca9137e8b2894a838e8
darkysadow <alexsmithshow@gmail.com>
2022-11-16 19:41:01 +0200

    Added Login navlink (if user not authorised) in Header component

--- src/components/header/userSearch/UserSearch.jsx
+++ src/components/header/userSearch/UserSearch.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import s from './../Header.module.css';
 import lupa from './../../../img/za.png';
 import face from './../../../img/face.jpg';
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { checkAuthMe } from "../../../redux/auth-reducer";
+import { NavLink } from "react-router-dom";
 
-const UserSearch = () => {
+{/*const UserSearch = () => {
     return (
         <div className={s.userSearch}>
             <div className={s.form}>
@@ -23,4 +27,45 @@ const UserSearch = () => {
     );
 }
 
-export default UserSearch;
+export default UserSearch;*/}
+
+class UserSearch extends React.Component {
+    componentDidMount() {
+        this.props.checkAuthMe();
+    }
+
+    render() {
+        if (!this.props.isAuth) {
+            return (<div className={s.userSearch}>
+                <NavLink to={'/login'}>Login</NavLink>
+            </div>)
+        } else {
+            return (
+                <div className={s.userSearch}>
+                    <div className={s.form}>
+                        <div className={s.form_search}>
+                            <input type="text" placeholder='Search...' />
+                        </div>
+                        <div className={s.form_button}>
+                            <input type="image" src={lupa} width='20px' height='20px' />
+                        </div>
+                    </div>
+                    <div className={s.profile}>
+                        <div className={s.profile_image}>
+                            <img src={face} alt="" width="30px" height="30px" />
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+    }
+}
+
+let mapStateToProps = (state) => {
+    return {
+        isAuth: state.auth.isAuthorized
+    }
+}
+export default compose(
+    connect(mapStateToProps, { checkAuthMe })
+)(UserSearch)
