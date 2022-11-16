32250866b877b8ab4aa9868baff01014a7522afa
darkysadow <alexsmithshow@gmail.com>
2022-11-16 17:23:02 +0200

    Refactored a Login component

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
+import s from "./Login.module.css";
 
 class Login extends React.Component {
     render() {
         return (<div className="container">
-            <div>
-                <div><input type="text" /></div>
-                <div><input type="password" /></div>
-                <div>
-                    <div><button>login</button></div>
-                    <div><button>register</button></div>
+            <div className={s.formContainer}>
+                <div className={s.inputsContainer}>
+                    <div className={s.inputLogin}><input type="text" placeholder="Login"/></div>
+                    <div className={s.inputPassword}><input type="password" placeholder="Password"/></div>
+                    <div className={s.buttonsContainer}>
+                        <div className={s.buttonLogin}><button>login</button></div>
+                        <div className={s.buttonRegister}><button>register</button></div>
+                    </div>
                 </div>
             </div>
         </div>)
